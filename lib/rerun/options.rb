8190e100374783eaa56729cb6b2f56257dd597fe
张翔宇 <xylinq@gmail.com>
2013-06-05 17:05:46 +0800

    modify default partten.

--- lib/rerun/options.rb
+++ lib/rerun/options.rb
@@ -6,7 +6,7 @@ $spec = Gem::Specification.load(File.join(libdir, "..", "rerun.gemspec"))
 
 module Rerun
   class Options
-    DEFAULT_PATTERN = "**/*.{rb,js,css,scss,sass,erb,html,haml,ru}"
+    DEFAULT_PATTERN = "**/*.{rb,js,coffee,css,scss,sass,erb,html,haml,ru,yml}"
 
     DEFAULTS = {
         :dir => ["."],
