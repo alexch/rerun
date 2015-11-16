66978621b50a0283e608ffde023d038ccc47dbf1
Christian Haase <c.haase@ifu.com>
2015-11-16 12:05:52 +0100

    Add *.c to default pattern

--- lib/rerun/options.rb
+++ lib/rerun/options.rb
@@ -8,7 +8,7 @@ $spec = Gem::Specification.load(File.join(libdir, "..", "rerun.gemspec"))
 
 module Rerun
   class Options
-    DEFAULT_PATTERN = "**/*.{rb,js,coffee,css,scss,sass,erb,html,haml,ru,yml,slim,md,feature}"
+    DEFAULT_PATTERN = "**/*.{rb,js,coffee,css,scss,sass,erb,html,haml,ru,yml,slim,md,feature,c}"
     DEFAULT_DIRS = ["."]
 
     DEFAULTS = {
